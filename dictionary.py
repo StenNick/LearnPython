24b1a28fcf203783f64d3bf2f9cdb6b07ca76bf4
StenNick <stennick01@gmail.com>
2018-08-31 18:18:51 +0300

    while, if, else

--- dictionary.py
+++ dictionary.py
@@ -132,3 +132,121 @@ list_aliens = [alien_1, alien_2, alien_3, alien_4]
 for unit in list_aliens:
     for valueUnit in unit.values():
         print(valueUnit)
+
+#--- Список в Словаре
+
+pizza = {
+    "cheese" : "macarela",
+    "salat"  : "iceberg",
+    "pepper" : "red",
+    "toppings": ["mushrooms", "extra cheese", "solt", "meat"]
+}
+
+for items in pizza['toppings']:
+    print("\t" + items)
+        # mushrooms
+        # extra cheese
+        # solt
+        # meat
+
+superCars = {
+    "germany" : ["BMW", "Mercedes", "Wolsvagen"],
+    "USA"   : ["Ford", "Dodge"],
+    "France" : ["Citroen", "Reno"],
+    "Italia" : ["Buggati", "Lambordginy"]
+}
+
+for countryName, carName in superCars.items():
+    print("\n" + countryName.title() + " country who makes cars")
+    for itemCar in carName:
+        print("\t" + itemCar.title())
+# Germany country who makes cars
+#         Bmw
+#         Mercedes
+#         Wolsvagen
+
+# Usa country who makes cars
+#         Ford
+#         Dodge
+
+# France country who makes cars
+#         Citroen
+#         Reno
+
+# Italia country who makes cars
+#         Buggati
+#         Lambordginy
+
+
+
+#---- Словарь в словаре 
+
+students = {
+    "Jordan" : {
+        "firstName" : "Jordan",
+        "lastName"  : "Morgan",
+        "location"  : "New-Jercy",
+        "age"   : 25
+    },
+    "Shon" : {
+        "firstName" : "Shon",
+        "lastName"  : "Andersson",
+        "location"  : "New-York",
+        "age"   : 12
+    },
+    "Bill" : {
+        "firstName" : "Bill",
+        "lastName"  : "Shewanian",
+        "location"  : "Detroit",
+        "age"   : 30
+    },
+    "Mayble" : {
+        "firstName" : "Mayble",
+        "lastName"  : "Pience",
+        "location"  : "Gravity-Falls",
+        "age"   : 13
+    },
+    "Deeper" : {
+        "firstName" : "Deeper",
+        "lastName"  : "Pience",
+        "location"  : "Gravity-Falls",
+        "age"   : 14
+    }
+}
+
+#--- сделаем перебор значений у словарей в словаре
+
+for userName, userInfo in students.items(): # перебрали свойства Главного словаря
+    print("\n" + "informantion about student " + str(userName)) # вывели ключи главного словаря
+    for information in userInfo.values(): # в этом цикле прошлись по всем Значениям подСловаря и вывели их
+        print("\t" + str(information))
+        
+# informantion about student Jordan     
+#         Jordan                        
+#         Morgan                        
+#         New-Jercy                     
+#         25                            
+                                      
+# informantion about student Shon       
+#         Shon                          
+#         Andersson                     
+#         New-York                      
+#         12                            
+                                      
+# informantion about student Bill       
+#         Bill                          
+#         Shewanian                     
+#         Detroit                       
+#         30                            
+                                      
+# informantion about student Mayble     
+#         Mayble                        
+#         Pience                        
+#         Gravity-Falls                 
+#         13                            
+                                      
+# informantion about student Deeper     
+#         Deeper                        
+#         Pience                        
+#         Gravity-Falls                 
+#         14                            
---
+++ while.py
@@ -0,0 +1,64 @@
+# message = input("Write me something here.. ")
+# print(message)
+
+
+# age_user = int(input("How old are you? ")) # int() переводит из строки в число. ибо если на указать Python что при вводе должно быть явное число, он перевед все в строку
+# print(age_user)
+
+# current_number = 1
+# while current_number <=5:
+#     print(current_number)
+#     current_number += 1
+#     # 1 2 3 4 5
+
+
+# prompt = "\n tell me you number phone "
+# message = ""
+# while message != "911":
+#     message = str(input(prompt))
+#     print(message)
+
+
+# your_visited_city = "\n City where you to be "
+
+# while True:
+#     city = input(your_visited_city)
+
+#     if city == 'Moscow':
+#         break
+#     else:
+#         print("You visited " + city + " city")
+
+
+
+
+pets = ['dog', 'cat', 'cat', 'goldfish' 'parot', 'cat', 'mouse', 'dog']
+print(pets)
+
+while 'cat'in pets:
+    pets.remove('cat')
+
+print(pets)
+
+listOfPeople = {
+    "Mishone" : {
+        "addres" : ["Moscow", "street is Mayskay 12", "10 level", "70 float"],
+        "secondName" : "Andersson",
+        "sex" : "male",
+        "job" : "dancer"
+    },
+    "Alen" : {
+        "addres" : ["Vancover", "street is Milstone 77", "road 909", "number house is 707"],
+        "secondName" : "Peterson",
+        "sex" : "man",
+        "job" : "policmen"
+    },
+    "Greg" : {
+        "addres" : ["Detroit", "street is Main Road 1254"],
+        "secondName" : "Daracot",
+        "sex" : "man",
+        "job" : "fireman"
+    }
+}
+
+
